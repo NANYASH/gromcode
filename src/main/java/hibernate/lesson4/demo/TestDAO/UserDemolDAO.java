db1966422d8434a08987641a47b7d50926fe1a56
nanyash <miss.nanya80@gmail.com>
2018-07-08 17:18:46 +0300

    comment

---
+++ src/main/java/hibernate/lesson4/demo/TestDAO/UserDemolDAO.java
@@ -0,0 +1,24 @@
+package hibernate.lesson4.demo.TestDAO;
+
+
+
+import hibernate.lesson4.dao.UserDAO;
+import hibernate.lesson4.entity.User;
+import hibernate.lesson4.entity.UserType;
+
+public class UserDemolDAO {
+    public static void main(String[] args) {
+        UserDAO userDAO = new UserDAO();
+
+        User user = new User();
+
+
+        user.setUserName("Anastasia");
+        user.setPassword("password");
+        user.setCountry("Kiev");
+        user.setUserType(UserType.ADMIN);
+
+        userDAO.save(user);
+
+    }
+}
