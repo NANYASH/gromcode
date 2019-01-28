9d696f07f771f33ee688c03edf2dab1dbd7ec85f
anastasiiasharaievska <miss.nanya80@gmail.com>
2019-01-28 21:02:39 +0200

    comment

--- src/main/java/jdbc/lesson3/lecture/ProductDAO.java
+++ src/main/java/jdbc/lesson3/lecture/ProductDAO.java
@@ -10,9 +10,9 @@ import java.util.List;
 
 public class ProductDAO {
 
-    private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lessons.cnrx1jkycv8d.us-east-2.rds.amazonaws.com:1521:ORCL";
-    private static final String USER = "main";
-    private static final String PASS = "asol1998";
+    private static final String DB_URL = "";
+    private static final String USER = "";
+    private static final String PASS = "";
 
     public Product save(Product product) {
 
